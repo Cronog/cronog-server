96c3b1ede60775eb2984528cd7beb8b3119e2067
mafealno <mafealno@gmail.com>
2022-07-28 15:51:28 -0300

    Alterado para que as envs fiquem no arquivo .env e não em arquivos JSON

--- src/connections/firebase.ts
+++ src/connections/firebase.ts
@@ -1,16 +1,25 @@
 import firebaseadmin from "firebase-admin";
 import { FirebaseApp, initializeApp } from 'firebase/app';
 
-const firebaseJson = require("../config/firebase.json");  
-const firebaseAppJson = require("../config/firebaseApp.json");  
-
 let db : firebaseadmin.firestore.Firestore
 
 export const connectFirebaseDb = () : firebaseadmin.firestore.Firestore => {
     
     if(!db){
         firebaseadmin.initializeApp({
-            credential: firebaseadmin.credential.cert(firebaseJson)
+            credential: firebaseadmin.credential.cert({
+                api_key : process.env[`FIREBASE${process.env.NODE_ENVIRONMENT}_API_KEY`],
+                type: process.env[`FIREBASE${process.env.NODE_ENVIRONMENT}_DB_TYPE`],
+                project_id: process.env[`FIREBASE${process.env.NODE_ENVIRONMENT}_PROJECT_ID`],
+                private_key_id: process.env[`FIREBASE${process.env.NODE_ENVIRONMENT}_DB_PRIVATE_KEY_ID`],
+                private_key: process.env[`FIREBASE${process.env.NODE_ENVIRONMENT}_DB_PRIVATE_KEY`],
+                client_email: process.env[`FIREBASE${process.env.NODE_ENVIRONMENT}_DB_CLIENT_EMAIL`],
+                client_id: process.env[`FIREBASE${process.env.NODE_ENVIRONMENT}_DB_CLIENT_ID`],
+                auth_uri: process.env[`FIREBASE${process.env.NODE_ENVIRONMENT}_DB_AUTH_URI`],
+                token_uri: process.env[`FIREBASE${process.env.NODE_ENVIRONMENT}_DB_TOKEN_URI`],
+                auth_provider_x509_cert_url: process.env[`FIREBASE${process.env.NODE_ENVIRONMENT}_DB_AUTH_PROVIDER_X509_CERT_URL`],
+                client_x509_cert_url: process.env[`FIREBASE${process.env.NODE_ENVIRONMENT}_DB_CLIENT_X509_CERT_URL`]
+            } as any)
         });
 
         db = firebaseadmin.firestore();
@@ -20,7 +29,14 @@ export const connectFirebaseDb = () : firebaseadmin.firestore.Firestore => {
 
 export const connectFirebaseAuth = () : FirebaseApp => {
     
-    const app = initializeApp(firebaseAppJson);
+    const app = initializeApp({
+        apiKey: process.env[`FIREBASE${process.env.NODE_ENVIRONMENT}_API_KEY`],
+        authDomain: process.env[`FIREBASE${process.env.NODE_ENVIRONMENT}_AUTH_DOMAIN`],
+        projectId: process.env[`FIREBASE${process.env.NODE_ENVIRONMENT}_PROJECT_ID`],
+        storageBucket: process.env[`FIREBASE${process.env.NODE_ENVIRONMENT}_STORAGE_BUCKET`],
+        messagingSenderId: process.env[`FIREBASE${process.env.NODE_ENVIRONMENT}_MESSAGING_SENDER_ID`],
+        appId: process.env[`FIREBASE${process.env.NODE_ENVIRONMENT}_APP_ID`]
+    });
 
     return app
 }
