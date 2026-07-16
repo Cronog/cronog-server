f8ba52beb3edf42138fb08dec6ba9b88795936b3
Marcelo Fernando Alves Nogueira <mafealno@gmail.com>
2026-07-15 21:48:42 -0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 require('dotenv').config();
+
+import https from 'https';
+import fs from 'fs';
 import cors from "cors";
 import express from "express";
 import cronog from "./routes/cronog";
@@ -19,8 +22,13 @@ app.use(color);
 app.use(auth);
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Rodando na porta ${process.env.PORT || 5001}`);
-});
+const httpsOptions = {
+    cert: fs.readFileSync('./server.crt'),
+    key: fs.readFileSync('./server.key'),
+  };
+  
+  https.createServer(httpsOptions, app).listen(PORT, () => {
+    console.log(`Servidor rodando com HTTPS na porta ${PORT}`);
+  });
 
 export default app;
